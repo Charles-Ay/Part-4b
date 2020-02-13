0b7d28a7115ed521be4e4be2c09ed9694aa34f85
Charles Ayeni <charles20020930@gmail.com>
2020-02-13 17:47:42 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Part_4b
+{
+    partial class frmRandom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterValue = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.btnDbl = new System.Windows.Forms.Button();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnterValue
+            // 
+            this.lblEnterValue.AutoSize = true;
+            this.lblEnterValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterValue.Location = new System.Drawing.Point(12, 9);
+            this.lblEnterValue.Name = "lblEnterValue";
+            this.lblEnterValue.Size = new System.Drawing.Size(367, 24);
+            this.lblEnterValue.TabIndex = 0;
+            this.lblEnterValue.Text = "Enter a minimum and maximum value.";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(12, 52);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(151, 20);
+            this.lblMin.TabIndex = 1;
+            this.lblMin.Text = "Minimum Number:";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(12, 87);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(155, 20);
+            this.lblMax.TabIndex = 2;
+            this.lblMax.Text = "Maximum Number:";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(169, 54);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 20);
+            this.txtMin.TabIndex = 3;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(173, 87);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 20);
+            this.txtMax.TabIndex = 4;
+            // 
+            // btnDbl
+            // 
+            this.btnDbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDbl.Location = new System.Drawing.Point(151, 110);
+            this.btnDbl.Name = "btnDbl";
+            this.btnDbl.Size = new System.Drawing.Size(122, 40);
+            this.btnDbl.TabIndex = 5;
+            this.btnDbl.Text = "Random Double";
+            this.btnDbl.UseVisualStyleBackColor = true;
+            this.btnDbl.Click += new System.EventHandler(this.btnDbl_Click);
+            // 
+            // btnInt
+            // 
+            this.btnInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInt.Location = new System.Drawing.Point(23, 110);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(122, 40);
+            this.btnInt.TabIndex = 6;
+            this.btnInt.Text = "Random Interger";
+            this.btnInt.UseVisualStyleBackColor = true;
+            this.btnInt.Click += new System.EventHandler(this.btnInt_Click);
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum.Location = new System.Drawing.Point(70, 166);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(156, 24);
+            this.lblNum.TabIndex = 7;
+            this.lblNum.Text = "Random Number";
+            this.lblNum.Click += new System.EventHandler(this.lblNum_Click);
+            // 
+            // frmRandom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 232);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.btnDbl);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblEnterValue);
+            this.Name = "frmRandom";
+            this.Text = "Random Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterValue;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Button btnDbl;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Label lblNum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_4b
+{
+    public partial class frmRandom : Form
+    {
+        Random generator = new Random();
+
+        
+        int maxI;
+        int minI;
+        int randNumI;
+        double randNumD;
+        string randIS;
+        double newDub;
+        double finalDub;
+        int zero;
+        
+      
+
+        public frmRandom()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            
+            maxI = System.Convert.ToInt32(txtMax.Text);
+            minI = System.Convert.ToInt32(txtMin.Text);
+            randNumI = generator.Next(minI, maxI);
+            randIS = System.Convert.ToString(randNumI);
+            lblNum.Text = randIS;
+            
+        
+        }
+
+        private void btnDbl_Click(object sender, EventArgs e)
+        {
+            
+            maxI = System.Convert.ToInt32(txtMax.Text);
+            minI = System.Convert.ToInt32(txtMin.Text);
+            randNumI = generator.Next(minI, maxI);
+            newDub = generator.NextDouble();
+            randNumD = System.Convert.ToDouble(randNumI);
+            zero = generator.Next(0, 2);
+            finalDub = ((randNumD * newDub) + zero);
+            randIS = System.Convert.ToString(finalDub);
+            lblNum.Text = randIS;
+            
+          
+        }
+
+        private void lblNum_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
